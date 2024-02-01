b018e517e0c94b0f377c4319ada2146ae470aa84
lehrmanaidins <lehrmanaidin+github.com@gmail.com>
2024-02-01 08:48:05 -0500

    Works

--- login.cpp
+++ login.cpp
@@ -14,7 +14,8 @@ bool isValidUsername(const std::string& input) {
 
     for (char ch : input) {
         if (validChars.find(ch) == std::string::npos) {
-            std::cout << "\rEntered Username is not Alphanumeric." << std::endl;
+            std::cout << "\rEntered Username is not Alphanumeric.\n";
+            std::cout << "Please Enter an Alphanumeric Username." << std::endl;
             return false;
         }
     }
@@ -22,7 +23,7 @@ bool isValidUsername(const std::string& input) {
     return true;
 }
 
-std::pair<std::string, std::string> getUsernameAndPassword() {
+std::pair<std::string, std::string> getUsernameAndPasswordFromUser() {
     std::cout << "===== User Login (Username has to be Alphanumeric) =====" << std::endl;
 
     std::string username;
@@ -38,18 +39,18 @@ std::pair<std::string, std::string> getUsernameAndPassword() {
     return {username, password};
 }
 
-
-
 std::pair<std::string, std::string> parseShadowLine(std::string line) {
-    if (line.at(0) != ':') {
-        throw "ValueError: Shadow File Corrupted";
-    }
+    /*if (line.at(0) != ':') {
+        throw "Tried Parsing a Comment";
+    }*/
     
-    std::size_t usernameStartPos = line.find(':', 1);
+    std::size_t usernameStartPos = line.find(':', 0) + 1;
     std::size_t usernameEndPos = line.find(':', usernameStartPos + 1);
-    
-    std::string username = line.substr(usernameStartPos + 1, usernameEndPos - usernameStartPos - 1);
-    std::string password = line.substr(usernameEndPos + 1, line.size() - username.size() - 3);
+    std::string username = line.substr(usernameStartPos, usernameEndPos - usernameStartPos);
+
+    std::size_t passwordStartPos = usernameEndPos + 2;
+    std::size_t passwordEndPos = line.find(':', passwordStartPos + 1);
+    std::string password = line.substr(passwordStartPos, passwordEndPos - passwordStartPos);
 
     return {username, password};
 }
@@ -59,17 +60,54 @@ std::size_t hashPassword(const std::string password) {
     return hasher(password);
 }
 
-int main() {
-    
-    std::ifstream file("shadow.txt");
-    std::string line;
-    getline(file, line);
+bool isComment(std::string line) {
+    int i = 0;
+    while (line.at(i) == ' ') {
+        i++;
+    }
+    i++;
+    return line.at(i) == '#';
+}
 
-    std::pair<std::string, std::string> credentials = parseShadowLine(line);
+bool loginCredentialsAreCorrect(std::string enteredUsername, std::string enteredPassword, std::ifstream& file) {
+    std::string line;
+    while (getline(file, line)) {
+        if (isComment(line)) {
+            continue;
+        }
+
+        std::pair<std::string, std::string> storedUsernameAndPasswordHash = parseShadowLine(line);
+
+        std::string storedUsername = storedUsernameAndPasswordHash.first;
+        std::string storedPasswordHash = storedUsernameAndPasswordHash.second;
+
+        if (storedUsername != enteredUsername) {
+            continue;
+        }
+
+        std::string enteredPasswordHash = std::to_string(hashPassword(enteredPassword));
+        if (storedPasswordHash == enteredPasswordHash) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    std::ifstream file("shadow.txt");
+
+    std::pair<std::string, std::string> enteredCredentials = getUsernameAndPasswordFromUser();
+    std::string enteredUsername = enteredCredentials.first;
+    std::string enteredPassword = enteredCredentials.second;
+
+    bool loginCorrect = loginCredentialsAreCorrect(enteredUsername, enteredPassword, file);
+
+    std::cout << loginCorrect << std::endl;
     
-    std::cout << "Username: " << credentials.first << "\n";
-    std::cout << "Password: " << credentials.second << "\n";
-    std::cout << "Hash: " << hashPassword(credentials.second) << std::endl;
+    // std::cout << "Username: " << enteredCredentials.first << "\n";
+    // std::cout << "Password: " << enteredCredentials.second << "\n";
+    // std::cout << "Hash: " << hashPassword(enteredCredentials.second) << std::endl;
 
     file.close();
     return 0;
